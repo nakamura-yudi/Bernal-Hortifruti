d95ba328e197bd7579d8469c61d5d26c42f1827c
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2026-05-26 23:05:38 -0300

    ajuste nginx

--- frontend/docker-entrypoint.d/40-generate-env.sh
+++ frontend/docker-entrypoint.d/40-generate-env.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -eu
 
+# Substitui ${API_BASE_URL} no env-config.js (lido pelo browser)
 envsubst '${API_BASE_URL}' \
   < /usr/share/nginx/html/env-config.js \
   > /tmp/env-config.js
-
 mv /tmp/env-config.js /usr/share/nginx/html/env-config.js
+
+# Substitui ${API_ORIGIN} no nginx.conf e gera o default.conf final
+# Usa lista explícita de variáveis para não tocar nas variáveis do próprio nginx ($uri, $remote_addr, etc.)
+envsubst '${API_ORIGIN}' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
